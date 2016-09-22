67ddb7d6dc27e13fd0426af970c367efa6766efd
whfkadl <whfkadl@naver.com>
2016-09-22 09:39:12 +0900

    Add files via upload

---
+++ while.py
@@ -0,0 +1,8 @@
+uid="2015010425"
+while True:
+    id_input=input("학생증을 체크해주세요")
+    if id_input==uid:
+        print(id_input, '님 인증되었습니다.')
+    else:
+        print(id_input, "님 인증되지 않았습니다.")
+ 
